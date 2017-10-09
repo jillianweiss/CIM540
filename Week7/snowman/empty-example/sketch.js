7fa19b408ac1472f315c3078aa0396eb1fe618f4
Jillian Weiss <jill_weiss@aim.com>
2017-10-09 19:03:06 -0400

    updates
    moving eyes, buttons

--- Week7/snowman/empty-example/sketch.js
+++ Week7/snowman/empty-example/sketch.js
@@ -7,20 +7,32 @@ var buttonColor ="blue";
 var noseColor = "orange";
 var smBall = 120;
 var lgBall = 180;
+var bgColor = 0;
+
+var buttonRedColor, buttonGreenColor, buttonBlueColor;
 
 
 function setup(){
+    
     createCanvas(1000,700);
-    background(179, 253, 255);
+    bgColor = color(179, 253, 255);
+    
 
     posX = width/2;
     posY = height/2;
+    
+    var redText = createP("Red");
+    buttonRedColor = createSlider(0, 255, 0);
+    var greenText = createP("Green");
+    buttonGreenColor = createSlider(0, 255, 0);
+    var blueText = createP("Blue");
+    buttonBlueColor = createSlider(0, 255, 0);
     
 }
 
 function draw(){
     
-    console.log("mouseX: " + mouseX + " mouseY: " + mouseY);
+    background(bgColor);
     
     posX = mouseX;
     posY = mouseY;
@@ -35,24 +47,26 @@ function draw(){
     ellipse(posX, posY + 150, lgBall, lgBall);
     
     //Eyes
+    var eyeMoveX = map(mouseX, 0, width, -15, 15);
+    var eyeMoveY = map(mouseY, 0, height, -15, 15);
     fill(eyeColor);
-    ellipse(posX - 30, posY - 20, 10, 10);
+    ellipse(posX - 25 + eyeMoveX, posY - 20 + eyeMoveY, 15, 15);
     
     fill(eyeColor);
-    ellipse(posX + 30, posY - 20, 10, 10);
+    ellipse(posX + 25 + eyeMoveX, posY - 20 + eyeMoveY, 15, 15);
     
     //Nose
-    fill(noseColor);
+    fill(noseColor);   
     triangle(posX - 30, posY + 10, posX, posY, posX, posY + 10);
     
     //Buttons
-    fill(buttonColor);
+    fill(buttonRedColor.value(), buttonGreenColor.value(), buttonBlueColor.value());
     ellipse(posX, posY + 120, 20, 20);
     
-    fill(buttonColor);
+    fill(buttonRedColor.value(), buttonGreenColor.value(), buttonBlueColor.value());
     ellipse(posX, posY + 150, 20, 20);
     
-    fill(buttonColor);
+    fill(buttonRedColor.value(), buttonGreenColor.value(), buttonBlueColor.value());
     ellipse(posX, posY + 180, 20, 20);
     
     //Mouth
@@ -71,4 +85,10 @@ function draw(){
     fill(mouthColor);
     ellipse(posX + 20, posY + 25, 5, 5);
 
+}
+
+function mousePressed(){
+    
+    bgColor = color(89, 251, 255);
+    
 }
