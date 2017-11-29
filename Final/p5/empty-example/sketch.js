0ace2e92a96b220e9aee1a237fd158cbd4f25e03
Jillian Weiss <jill_weiss@aim.com>
2017-11-29 12:52:03 -0500

    loaded csv table

--- Final/p5/empty-example/sketch.js
+++ Final/p5/empty-example/sketch.js
@@ -1,26 +1,30 @@
-var movieArray = [];
+var movies;
+
+function preload() {
+    movies = loadTable("../../assets/2017movies.csv", "csv", "header");
+}
 
 function setup() {
     createCanvas(1000, 1000);
 
-    movieArray.push(new movie(10, ))
+    print(movies.getRowCount() + " total rows in table");
+    print(movies.getColumnCount() + " total columns in table");
+
+    print(movies.getColumn("movie"));
+
+    for (var r = 0; r < movies.getRowCount(); r++) {
+        for (var c = 0; c < movies.getColumnCount(); c++) {
+            print(movies.getString(r, c));
+        }
+    }
 }
+
 
 function draw() {
 
 
 }
 
+function movie() {
 
-function movie(tempX, tempY, tempBarSize, tempColor) {
-    this.x = tempX;
-    this.y = tempY;
-    this.barWidth = tempBarWidth;
-    this.barHeight = tempBarHeight;
-    this.setFill = tempColor;
-
-    this.display = function () {
-        fill(this.setFill);
-        rect(this.x, this.y, this.barWidth, this.barHeight);
-    }
 }
